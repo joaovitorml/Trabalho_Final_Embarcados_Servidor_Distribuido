9e64548018f72b839d5c1c668cace0784c047e21
aquiles <jjoseaquiless@gmail.com>
2020-12-16 23:08:37 -0300

    error solved, init file store
    Co-authored-by: João Vítor Morandi <joao.lemos4000@gmail.com>

---
+++ include/led.h
@@ -0,0 +1,8 @@
+#ifndef LED_H
+#define LED_H
+
+void init_led();
+
+void set_led_state(int state);
+
+#endif
---
+++ include/nvs_config.h
@@ -0,0 +1,5 @@
+#ifndef NVS_CONFIG_H
+#define NVS_CONFIG_H
+void grava_valor_nvs(int32_t valor);
+int32_t le_valor_nvs();
+#endif
